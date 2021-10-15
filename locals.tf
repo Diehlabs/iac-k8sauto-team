d6d0821855aa29bc17a349d4e7aab7c66a3e28b3
Chris Diehl <cultclassik@gmail.com>
2021-10-15 13:59:12 -0500

    fix typo

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
       k8sauto-tkg-azure  = "IaC repo for TKG on Azure"
       k8sauto-aks        = "IaC repo for private AKS"
       k8sauto-core-azure = "IaC repo for re-usable Azure resources"
-      k8sauto-ajs-jump   = "IaC repo for a jump box VM to communicate with AKS cluster"
+      k8sauto-aks-jump   = "IaC repo for a jump box VM to communicate with AKS cluster"
     }
   }
 }
