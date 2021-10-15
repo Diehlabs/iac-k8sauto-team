3ad43eedf78fc8a35eb93796ebc975bd5fb3a8f8
Chris Diehl <cultclassik@gmail.com>
2021-10-15 16:16:56 -0500

    add deployment repo

--- locals.tf
+++ locals.tf
@@ -1,10 +1,12 @@
 locals {
   repos = {
     iac = {
-      k8sauto-tkg-azure  = "IaC repo for TKG on Azure"
-      k8sauto-aks        = "IaC repo for private AKS"
-      k8sauto-core-azure = "IaC repo for re-usable Azure resources"
-      k8sauto-aks-jump   = "IaC repo for a jump box VM to communicate with AKS cluster"
+      k8sauto-tkg-azure   = "IaC repo for TKG on Azure"
+      k8sauto-aks         = "IaC repo for private AKS"
+      k8sauto-aks-deploys = "Deployments for AKS cluster"
+      k8sauto-core-azure  = "IaC repo for re-usable Azure resources"
+      k8sauto-aks-jump    = "IaC repo for a jump box VM to communicate with AKS cluster"
+
     }
   }
 }
